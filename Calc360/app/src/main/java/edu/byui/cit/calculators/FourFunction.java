1de9c7a6954060a5ddd9d32a3977874382177dbf
grook14 <roo15003@byui.edu>
2018-01-31 11:08:40 -0700

    Pushing Java Main

---
+++ Calc360/app/src/main/java/edu/byui/cit/calculators/FourFunction.java
@@ -0,0 +1,15 @@
+package edu.byui.cit.calculators;
+
+import java.text.NumberFormat;
+
+import edu.byui.cit.calc360.CalcFragment;
+
+
+/**
+ * Created by GavinrRook on 1/30/18.
+ */
+
+public class FourFunction extends CalcFragment{
+	private NumberFormat;
+
+}
