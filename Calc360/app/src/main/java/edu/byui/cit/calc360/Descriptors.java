5679d5c075d3210adb3c62418a282d76b0be0a39
Trevor <cut12005@byui.edu>
2018-02-02 09:37:47 -0700

    Commit Mike and Trevor

--- Calc360/app/src/main/java/edu/byui/cit/calc360/Descriptors.java
+++ Calc360/app/src/main/java/edu/byui/cit/calc360/Descriptors.java
@@ -25,7 +25,7 @@ final class Descriptors {
 				new CalcDescriptor(1026, R.string.foreignFuel, R.drawable.fuel_pump, ForeignFuel.class),
 				new CalcDescriptor(1036, R.string.currencyExch, R.drawable.currency_exchange, CurrencyExchange.class),
 				new CalcDescriptor(1002, R.string.tip, R.drawable.tip, Tip.class),
-				new CalcDescriptor(1060, R.string.fuelReimburse, R.drawable.miles_driven1,MilesDriven.class),
+				new CalcDescriptor(1060, R.string.fuelReimburse, R.mipmap.miles_driven,MilesDriven.class),
 			}),
 			new GroupDescriptor(303, R.string.finance, R.drawable.folder_finance, new CalcDescriptor[]{
 				new CalcDescriptor(1013, R.string.compoundInterest, R.drawable.compound_interest, CompoundInterest.class),
