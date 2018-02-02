52dee899173bd36983776f2585475896f5a5ff17
grook14 <roo15003@byui.edu>
2018-02-02 11:58:53 -0700

    FINISHED EVERYTHING GAAHHHH

--- Calc360/app/src/main/java/edu/byui/cit/calc360/Descriptors.java
+++ Calc360/app/src/main/java/edu/byui/cit/calc360/Descriptors.java
@@ -63,6 +63,7 @@ final class Descriptors {
 				new CalcDescriptor(1046, R.string.cone, R.drawable.cone, Cone.class),
 				new CalcDescriptor(1058, R.string.rectPrism, R.drawable.box, RectangularPrism.class),
 				new CalcDescriptor(1034, R.string.pyramid, R.drawable.pyramid, Pyramid.class),
+					new CalcDescriptor(1111, R.string.multiplication2, R.drawable.multiplication, FourFunction.class),
 				/*
 				new CalcDescriptor(?, "GCM and LCM", R.drawable.gcmlcm, GCMandLCM.class),
 				 */
