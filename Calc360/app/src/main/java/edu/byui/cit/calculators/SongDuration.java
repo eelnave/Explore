44c651c7ad6578445a170473fba9a89b463614ee
Cameron <osb13004@byui.edu>
2018-02-07 11:16:39 -0700

    still incomplete. Added working calculation for song Duration. Still need to edit necessary xml files and format the layout correctly.

---
+++ Calc360/app/src/main/java/edu/byui/cit/calculators/SongDuration.java
@@ -0,0 +1,87 @@
+package edu.byui.cit.calculators;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.text.NumberFormat;
+import edu.byui.cit.calc360.CalcFragment;
+import edu.byui.cit.calc360.R;
+import edu.byui.cit.model.Consumer;
+import edu.byui.cit.text.ControlWrapper;
+import edu.byui.cit.text.EditInt;
+import edu.byui.cit.text.EditWrapper;
+import edu.byui.cit.text.TextWrapper;
+
+//Song calculation
+//get tempo, time signature, no of measures
+//multiply time signature (top) * no of measures / tempo (beatsPerMinute)
+//display in seconds if less than a minute, minutes if less than an hour, hour otherwise
+//do not allow negative inputs
+//possibly allow full time or just beats per measure
+//cap if needed
+
+
+
+public final class SongDuration extends CalcFragment {
+	
+	// Each of these variables is a reference to
+	// one of the text fields in this calculator.
+	
+	private EditInt totalMeasures;
+	private EditInt beatsPerMinute;
+	private EditInt timeSignature;
+	private TextWrapper songTime;
+
+
+	public SongDuration() {
+		// Call the constructor in the parent class.
+		super();
+	}
+
+
+	protected View createView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this calculator.
+		View view = inflater.inflate(R.layout.SongDuration, container, false);
+
+		// Get a reference to each of the text fields in this calculator.
+		totalMeasures = new EditInt(view, R.id.totalMeasures, this);
+		beatsPerMinute = new EditInt(view, R.id.beatsPerMinute, this);
+		timeSignature = new EditInt(view, R.id.timeSignature, this);
+		songTime = new TextWrapper(view, R.id.songTime);
+
+		EditWrapper[] inputs = { totalMeasures, beatsPerMinute };
+		ControlWrapper[] toClear = { totalMeasures, songTime, beatsPerMinute};
+		initialize(view, inputs, toClear, R.id.btnClear);
+		return view;
+	}
+	
+	
+	@Override
+	protected void compute() {
+		if (totalMeasures.notEmpty() && beatsPerMinute.notEmpty()) {
+			double measures = totalMeasures.getInt();
+			double bpm = beatsPerMinute.getInt();
+			double timeSig = timeSignature.getInt();
+			//returns song duration in hours, minutes, seconds
+			double songDur = timeSig * (measures / bpm);
+
+			int time = (int)(songDur * 60);
+
+			int hour = time/3600;
+			time %= 3600;
+			int min = time/60;
+			time %= 60;
+			int sec = time;
+			String songDurText = hour + ":" + min + ":" + sec;
+
+			songTime.setText(songDurText);
+		}
+		else {
+			songTime.clear();
+		}
+	}
+}
