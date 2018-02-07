f9fe49c648739c7ce902b92d059cb1893f48cb6f
JoshK_000 <spr12011@byui.edu>
2018-02-07 11:42:49 -0700

    Putting in a clear variable.

--- Calc360/app/src/main/java/edu/byui/cit/calculators/Subnet.java
+++ Calc360/app/src/main/java/edu/byui/cit/calculators/Subnet.java
@@ -72,127 +72,140 @@ public class Subnet extends CalcFragment {
 
 	@Override
 	protected void compute() {
-		int input1 = ip1.getInt();
-		int input2 = ip2.getInt();
-		int input3 = ip3.getInt();
-		int input4 = ip4.getInt();
-		int input5 = ip5.getInt();
-		int input6 = ip6.getInt();
-		int input7 = ip7.getInt();
-		int input8 = ip8.getInt();
-		int sub = 0;
-		int zero = 0;
+		if (ip1.notEmpty() && ip2.notEmpty() && ip3.notEmpty() && ip4.notEmpty() && ip5.notEmpty()
+				&& ip6.notEmpty() && ip7.notEmpty() && ip8.notEmpty()) {
+			int input1 = ip1.getInt();
+			int input2 = ip2.getInt();
+			int input3 = ip3.getInt();
+			int input4 = ip4.getInt();
+			int input5 = ip5.getInt();
+			int input6 = ip6.getInt();
+			int input7 = ip7.getInt();
+			int input8 = ip8.getInt();
+			int sub = 0;
+			int zero = 0;
 
-		if (input5 >= 255) {
-			if (input6 >= 255) {
-				if (input7 >= 255) {
-					if (input8 >= 255) {
+			if (input5 >= 255) {
+				if (input6 >= 255) {
+					if (input7 >= 255) {
+						if (input8 >= 255) {
 
+						}
+						else {
+							if (input8 == 0) {
+								zero = 254;
+								sub = 1;
+							}
+							else if (input8 == 128) {
+								zero = 126;
+								sub = 2;
+							}
+							else if (input8 == 192) {
+								zero = 62;
+								sub = 4;
+							}
+							else if (input8 == 224) {
+								zero = 30;
+								sub = 8;
+							}
+							else if (input8 == 240) {
+								zero = 14;
+								sub = 16;
+							}
+							else if (input8 == 248) {
+								zero = 6;
+								sub = 32;
+							}
+							else if (input8 == 252) {
+								zero = 2;
+								sub = 64;
+							}
+
+						}
 					}
 					else {
-						if (input8 == 0) {
-							zero = 254;
+						if (input7 == 0) {
+							zero = 65534;
 							sub = 1;
 						}
-						else if (input8 == 128) {
-							zero = 126;
+						else if (input7 == 128) {
+							zero = 32766;
 							sub = 2;
 						}
-						else if (input8 == 192) {
-							zero = 62;
+						else if (input7 == 192) {
+							zero = 16382;
 							sub = 4;
 						}
-						else if (input8 == 224) {
-							zero = 30;
+						else if (input7 == 224) {
+							zero = 8190;
 							sub = 8;
 						}
-						else if (input8 == 240) {
-							zero = 14;
+						else if (input7 == 240) {
+							zero = 4094;
 							sub = 16;
 						}
-						else if (input8 == 248) {
-							zero = 6;
+						else if (input7 == 248) {
+							zero = 2046;
 							sub = 32;
 						}
-						else if (input8 == 252) {
-							zero = 2;
+						else if (input7 == 252) {
+							zero = 1022;
 							sub = 64;
+						}
+						else if (input7 == 254) {
+							zero = 510;
+							sub = 128;
 						}
 
 					}
 				}
 				else {
-					if (input7 == 0) {
-						zero = 65534;
+					if (input6 == 0) {
+						zero = 16777214;
 						sub = 1;
 					}
-					else if (input7 == 128) {
-						zero = 32766;
+					else if (input6 == 128) {
+						zero = 8388606;
 						sub = 2;
 					}
-					else if (input7 == 192) {
-						zero = 16382;
+					else if (input6 == 192) {
+						zero = 4194302;
 						sub = 4;
 					}
-					else if (input7 == 224) {
-						zero = 8190;
+					else if (input6 == 224) {
+						zero = 2097150;
 						sub = 8;
 					}
-					else if (input7 == 240) {
-						zero = 4094;
+					else if (input6 == 240) {
+						zero = 1048574;
 						sub = 16;
 					}
-					else if (input7 == 248) {
-						zero = 2046;
+					else if (input6 == 248) {
+						zero = 524286;
 						sub = 32;
 					}
-					else if (input7 == 252) {
-						zero = 1022;
+					else if (input6 == 252) {
+						zero = 262142;
 						sub = 64;
 					}
-					else if (input7 == 254) {
-						zero = 510;
+					else if (input6 == 254) {
+						zero = 131070;
 						sub = 128;
 					}
-
 				}
 			}
-			else {
-				if (input6 == 0) {
-					zero = 16777214;
-					sub = 1;
-				}
-				else if (input6 == 128) {
-					zero = 8388606;
-					sub = 2;
-				}
-				else if (input6 == 192) {
-					zero = 4194302;
-					sub = 4;
-				}
-				else if (input6 == 224) {
-					zero = 2097150;
-					sub = 8;
-				}
-				else if (input6 == 240) {
-					zero = 1048574;
-					sub = 16;
-				}
-				else if (input6 == 248) {
-					zero = 524286;
-					sub = 32;
-				}
-				else if (input6 == 252) {
-					zero = 262142;
-					sub = 64;
-				}
-				else if (input6 == 254) {
-					zero = 131070;
-					sub = 128;
-				}
-			}
+			numberOfSubnets.setText(sub);
+			availableHosts.setText(String.valueOf(zero));
 		}
-		numberOfSubnets.setText(sub);
-		availableHosts.setText(String.valueOf(zero));
+		else {
+			ip1.clear();
+			ip2.clear();
+			ip3.clear();
+			ip4.clear();
+			ip5.clear();
+			ip6.clear();
+			ip7.clear();
+			ip8.clear();
+		}
 	}
 }
