48b9f432dfd8a37ff8173e472280a02aac7b7b26
dustinrowbury.github.io <frisbeedust@hotmail.com>
2018-02-07 11:18:18 -0700

    Intial calculator layout

---
+++ Calc360/app/src/main/java/edu/byui/cit/calculators/StarExposure.java
@@ -0,0 +1,21 @@
+package edu.byui.cit.calculators;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.byui.cit.calc360.CalcFragment;
+
+
+/**
+ * Created by Frisbeedust on 2/7/2018.
+ */
+
+public class StarExposure extends CalcFragment {
+	@Override
+	protected View createView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstState) {
+		return null;
+	}
+}
