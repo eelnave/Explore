86e8c3779ed9c53b72570798db6dab68c7f08f06
MightyMormon <nic15021@byui.edu>
2018-02-14 03:02:38 -0700

    Here we go again

--- Calc360/app/src/main/java/edu/byui/cit/calculators/Age.java
+++ Calc360/app/src/main/java/edu/byui/cit/calculators/Age.java
@@ -1,19 +1,19 @@
 package edu.byui.cit.calculators;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import java.text.NumberFormat;
-import edu.byui.cit.calc360.CalcFragment;
-import edu.byui.cit.calc360.R;
-import edu.byui.cit.text.ControlWrapper;
-import edu.byui.cit.text.EditDecimal;
-import edu.byui.cit.text.EditInteger;
-import edu.byui.cit.text.EditWrapper;
-import edu.byui.cit.text.TextWrapper;
-import java.time.*;
-import java.time.temporal.ChronoUnit;
+		import android.os.Bundle;
+		import android.view.LayoutInflater;
+		import android.view.View;
+		import android.view.ViewGroup;
+
+		import java.text.NumberFormat;
+		import java.util.Calendar;
+		import edu.byui.cit.calc360.CalcFragment;
+
+		import edu.byui.cit.calc360.R;
+		import edu.byui.cit.text.ControlWrapper;
+		import edu.byui.cit.text.EditInteger;
+		import edu.byui.cit.text.EditWrapper;
+		import edu.byui.cit.text.TextWrapper;
 
 public final class Age extends CalcFragment {
 	private final NumberFormat fmtrDec;
@@ -41,7 +41,7 @@ public final class Age extends CalcFragment {
 		numberOne = new EditInteger(view, R.id.numberOne, this);
 		numberTwo = new EditInteger(view, R.id.numberThree, this);
 		numberThree = new EditInteger(view, R.id.numberThree, this);
-		exactAge= new TextWrapper(view, R.id.exactAge);
+		exactAge = new TextWrapper(view, R.id.exactAge);
 
 		EditWrapper[] inputs = { numberOne, numberTwo, numberThree };
 		ControlWrapper[] toClear = { numberOne, numberTwo, numberThree, exactAge };
@@ -49,23 +49,48 @@ public final class Age extends CalcFragment {
 		return view;
 	}
 
-	LocalDate start = LocalDate.of(numberThree, numberTwo, numberOne);
-	LocalDate end = LocalDate.now(); // use for age-calculation: LocalDate.now()
-	long years = ChronoUnit.YEARS.between(start, end);
 
-				@Override
-				protected void compute() {
+	@Override
+	protected void compute() {
 
+			Integer day = numberOne.getInt();
+			Integer month = numberTwo.getInt();
+			Integer year = numberThree.getInt();
+
+			Calendar now = Calendar.getInstance();
+			Calendar dateOfBirth = Calendar.getInstance();
+			int age;
+			int monthage;
+			int dayage;
+			String actualAge;
+			if (dateOfBirth.after(now)) {
+				throw new IllegalArgumentException(
+						"Can't be born in the future");
+			}
+			else {
+				int year1 = now.get(Calendar.YEAR);
+				int year2 = year;
+				age = year1 - year2;
+				int month1 = now.get(Calendar.MONTH);
+				int month2 = month;
+				if (month2 > month1) {
+					monthage = month1 - month2;
+					int day1 = now.get(Calendar.DAY_OF_MONTH);
+					int day2 = day;
+					if (day1 > day2) {
+						dayage = day2 - day1;
+					}
+				}
+				else if (month1 == month2) {
+					monthage = 0;
+					int day1 = now.get(Calendar.DAY_OF_MONTH);
+					int day2 = day;
+					if (day2 > day1) {
+						dayage = day2 - day1;
+					}
 				}
 			}
+
+			exactAge.setText(fmtrDec.format(age));
 		}
-
-//public class AgeCalculator {
-
-//public static int calculateAge(LocalDate birthDate, LocalDate currentDate) {
-//if ((birthDate != null) && (currentDate != null)) {
-//	return Period.between(birthDate, currentDate).getYears();
-//} else {
-//	return 0;
-//}
-//}
+	}
