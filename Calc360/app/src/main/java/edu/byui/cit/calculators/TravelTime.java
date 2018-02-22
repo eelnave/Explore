d1cc4fbceec8e9e23bdfdcf524d55f646b7fe635
BarzeeRex <barzeer@byui.edu>
2018-02-22 11:32:05 -0700

    Merge branches 'master' and 'star-exposure-james-dustin' of https://bitbucket.org/cit360byui/apps
    # Conflicts:
    #	Calc360/app/src/main/res/drawable-hdpi/folder_art.png
    #	Calc360/app/src/main/res/drawable-mdpi/folder_art.png
    #	Calc360/app/src/main/res/drawable-xhdpi/folder_art.png
    #	Calc360/app/src/main/res/drawable-xxhdpi/folder_art.png
    #	Calc360/app/src/main/res/drawable-xxxhdpi/folder_art.png
    #	Calc360/app/src/main/res/values/art.xml
    #	Calc360/images/folder_art.png

--- Calc360/app/src/main/java/edu/byui/cit/calculators/TravelTime.java
+++ Calc360/app/src/main/java/edu/byui/cit/calculators/TravelTime.java
@@ -2,14 +2,13 @@ package edu.byui.cit.calculators;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.text.NumberFormat;
-import java.text.SimpleDateFormat;
 
 import edu.byui.cit.calc360.CalcFragment;
 import edu.byui.cit.calc360.R;
@@ -17,7 +16,6 @@ import edu.byui.cit.text.ControlWrapper;
 import edu.byui.cit.text.EditDec;
 import edu.byui.cit.text.EditWrapper;
 import edu.byui.cit.text.SpinUnit;
-import edu.byui.cit.text.TextChangeHandler;
 import edu.byui.cit.text.TextWrapper;
 import edu.byui.cit.units.Length;
 import edu.byui.cit.units.Property;
@@ -31,21 +29,10 @@ public final class TravelTime extends CalcFragment {
 			KEY_DIST_UNITS = "TravelTime.distUnits",
 			KEY_SPE_UNITS = "TravelTime.speUnits";
 
-	private final NumberFormat fmtrDist, fmtrEffic;
-	private EditDec decDist, decSpe;
-	private SpinUnit spinDistUnits, spinSpeUnits;
+	private final NumberFormat fmtrInt = NumberFormat.getIntegerInstance();
+	private EditDec decDist, decSpeed;
+	private SpinUnit spinDistUnits, spinSpeedUnits;
 	private TextWrapper decTime;
-
-
-	public TravelTime() {
-		super();
-		fmtrDist = NumberFormat.getInstance();
-		fmtrEffic = NumberFormat.getInstance();
-		fmtrDist.setMinimumFractionDigits(0);
-		fmtrDist.setMaximumFractionDigits(1);
-		fmtrEffic.setMinimumFractionDigits(1);
-		fmtrEffic.setMaximumFractionDigits(1);
-	}
 
 
 	@Override
@@ -58,7 +45,7 @@ public final class TravelTime extends CalcFragment {
 		// Create a wrapper object for each EditText
 		// that appears in this calculator's layout.
 		decDist = new EditDec(view, R.id.decDist, this);
-		decSpe = new EditDec(view, R.id.decSpe, this);
+		decSpeed = new EditDec(view, R.id.decSpeed, this);
 
 		// Get the user's preferred units from the system
 		// preferences file and initialize each spinner.
@@ -66,15 +53,14 @@ public final class TravelTime extends CalcFragment {
 		spinDistUnits = new SpinUnit(act, view, R.id.spinDistUnits,
 				Length.getInstance(), R.array.ttDistUnits,
 				KEY_DIST_UNITS, this);
-		spinSpeUnits = new SpinUnit(act, view, R.id.spinSpeUnits,
+		spinSpeedUnits = new SpinUnit(act, view, R.id.spinSpeedUnits,
 				Speed.getInstance(), R.array.ttSpeedUnits,
 				KEY_SPE_UNITS, this);
 
 		decTime = new TextWrapper(view, R.id.decTime);
 
-		EditWrapper[] inputs = { decDist, decSpe };
-		//TextWrapper[] outputs = { decTime };
-		ControlWrapper[] toClear = { decDist, decSpe, decTime };
+		EditWrapper[] inputs = { decDist, decSpeed };
+		ControlWrapper[] toClear = { decDist, decSpeed, decTime };
 		initialize(view, inputs, toClear, R.id.btnClear);
 		return view;
 	}
@@ -83,7 +69,7 @@ public final class TravelTime extends CalcFragment {
 	@Override
 	protected void restorePrefs(SharedPreferences prefs) {
 		spinDistUnits.restore(prefs, Length.mile);
-		spinSpeUnits.restore(prefs, Speed.mph);
+		spinSpeedUnits.restore(prefs, Speed.mph);
 	}
 
 	@Override
@@ -92,81 +78,86 @@ public final class TravelTime extends CalcFragment {
 		// the user into the preferences file.
 		// Get from the spinners, the units chosen by the user.
 		spinDistUnits.save(editor);
-		spinSpeUnits.save(editor);
+		spinSpeedUnits.save(editor);
 	}
 
 
 	@Override
 	protected void compute() {
-		double dist = 0;
-		if (decDist.notEmpty()) {
-			dist = decDist.getDec();
-		}
-
-		if (dist > 0 && decSpe.notEmpty()) {
-			double spe = decSpe.getDec();
+		if (decDist.notEmpty() && decSpeed.notEmpty()) {
+			double dist = decDist.getDec();
+			double spe = decSpeed.getDec();
 
 			// Get from the spinners, the units that the user chose
-			// for inputting the distance and the volume of fuel.
+			// for inputting the distance and the average speed.
 			Unit distUnits = spinDistUnits.getSelectedItem();
-			Unit speUnits = spinSpeUnits.getSelectedItem();
+			Unit speedUnits = spinSpeedUnits.getSelectedItem();
 
-			// Get the length and volume properties so that they can
+			// Get the length and speed properties so that they can
 			// be used to convert values from one unit to another.
 			Property length = Length.getInstance();
 			Property speed = Speed.getInstance();
 
-			// If the user wants the results in miles per gallon,
-			// then convert, if necessary, the distance and volume
-			// of fuel entered by the user into miles and gallons.
-			if (speUnits.getID() == Speed.mph) {
-				dist = length.convert(Length.mile, dist, distUnits);
-				spe = speed.convert(Speed.mph, spe, speUnits);
-			}
+			dist = length.convert(Length.km, dist, distUnits);
+			spe = speed.convert(Speed.kmph, spe, speedUnits);
 
-			// If the user wants the results in kilometers per mile,
-			// then convert, if necessary, the distance and speed of
-			// distance entered by the user into kilometers and kmph.
-			else if (speUnits.getID() == Speed.kmph) {
-				dist = length.convert(Length.km, dist, distUnits);
-				spe = speed.convert(Speed.kmph, spe, speUnits);
-			}
+//			// If the user wants the results in miles per gallon,
+//			// then convert, if necessary, the distance and volume
+//			// of fuel entered by the user into miles and gallons.
+//			if (speedUnits.getID() == Speed.mph) {
+//				dist = length.convert(Length.mile, dist, distUnits);
+//				spe = speed.convert(Speed.mph, spe, speedUnits);
+//			}
+//
+//			// If the user wants the results in kilometers per mile,
+//			// then convert, if necessary, the distance and speed of
+//			// distance entered by the user into kilometers and kmph.
+//			else if (speedUnits.getID() == Speed.kmph) {
+//				dist = length.convert(Length.km, dist, distUnits);
+//				spe = speed.convert(Speed.kmph, spe, speedUnits);
+//			}
 
-			double time = dist / spe ;
-			int hours = (int) time;
-			int min = (int) ((time - hours) * 60);
-			if (hours == 1) {
-				if (min == 1) {
-					String result = hours + " hour " + min + " minute";
-					decTime.setText(result);
+			double time = dist / spe;  // In hours
+			int hours = (int)Math.floor(time);
+			int min = (int)Math.round((time - hours) * 60);
 
-				}
-				else {
-					String result = hours + " hour " + min + " minutes";
-					decTime.setText(result);
+			Resources res = getResources();
+			String result = fmtrInt.format(hours) + " " +
+					res.getQuantityString(R.plurals.hour, hours) + " " +
+					fmtrInt.format(min) + " " +
+					res.getQuantityString(R.plurals.minute, min);
+			decTime.setText(result);
 
-				}
-			}
-			else if (hours > 1) {
-				if (min == 1) {
-					String result = hours + " hours " + min + " minute";
-					decTime.setText(result);
-				}
-				else {
-					String result = hours + " hours " + min + " minutes";
-					decTime.setText(result);
-				}
-			}
-			else if (hours < 1){
-				if(min == 1) {
-					String result = hours + " hours " + min + " minute";
-					decTime.setText(result);
-				}
-				else {
-					String result = hours + " hours " + min + " minutes";
-					decTime.setText(result);
-				}
-			}
+//			if (hours == 1) {
+//				if (min == 1) {
+//					String result = hours + " hour " + min + " minute";
+//					decTime.setText(result);
+//				}
+//				else {
+//					String result = hours + " hour " + min + " minutes";
+//					decTime.setText(result);
+//				}
+//			}
+//			else if (hours > 1) {
+//				if (min == 1) {
+//					String result = hours + " hours " + min + " minute";
+//					decTime.setText(result);
+//				}
+//				else {
+//					String result = hours + " hours " + min + " minutes";
+//					decTime.setText(result);
+//				}
+//			}
+//			else if (hours < 1){
+//				if(min == 1) {
+//					String result = hours + " hours " + min + " minute";
+//					decTime.setText(result);
+//				}
+//				else {
+//					String result = hours + " hours " + min + " minutes";
+//					decTime.setText(result);
+//				}
+//			}
 
 
 			//}
