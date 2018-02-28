eee0c8d8bf9a3e960af7e1fe847d77f9f123993c
JoshK_000 <spr12011@byui.edu>
2018-02-28 12:25:11 -0700

    Creating the test and fixing it

--- Calc360/app/src/test/java/edu/byui/cit/calc360/ConsumerTest.java
+++ Calc360/app/src/test/java/edu/byui/cit/calc360/ConsumerTest.java
@@ -59,4 +59,21 @@ public final class ConsumerTest {
 		assertEquals(tipRate, Ratio.rate(tipAmt, cost), delta);
 		assertEquals(total, Tip.total(cost, taxAmt, tipAmt), delta);
 	}
+
+	@Test
+	public  void testNetflix() {
+		double price = 8.0;
+		double firstHour = 4.0;
+		double secondHour = 9.0;
+		double thirdHour = 3.0;
+		double firstMin = 22.0;
+		double secondMin = 45.0;
+		double time = NetflixComputing.time(firstHour,secondHour, thirdHour,firstMin,secondMin);
+		assertEquals(0.12, NetflixComputing.sum(price, time), delta);
+		assertEquals(10, NetflixComputing.movieLengthTwoHalf(firstHour), delta);
+		assertEquals(18, NetflixComputing.movieLengthTwo(secondHour), delta);
+		assertEquals(4.5, NetflixComputing.movieLengthOneHalf(thirdHour), delta);
+		assertEquals(16.5, NetflixComputing.episodeLengthFourFive(firstMin), delta);
+		assertEquals(16.65, NetflixComputing.episodeLengthTwoTwo(secondMin), delta);
+	}
 }
