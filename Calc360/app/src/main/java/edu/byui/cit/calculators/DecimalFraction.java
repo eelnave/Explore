a3ac45821c85eae56da087259a3ff572146679e4
Caliesha Stimpson <sti15002@byui.edu>
2018-03-02 23:44:06 -0700

    3/2/18 layout ready plus java pseudo code

--- Calc360/app/src/main/java/edu/byui/cit/calculators/DecimalFraction.java
+++ Calc360/app/src/main/java/edu/byui/cit/calculators/DecimalFraction.java
@@ -6,4 +6,23 @@ package edu.byui.cit.calculators;
  */
 
 public class DecimalFraction {
+
+	/*
+	* fraction -> decimal
+	* numerator/denominator
+	*
+	*
+	* decimal -> fraction
+	* if 1 decimal place then put over 10 (compare)
+	* 	loop 1-10
+	* 		if % of (10/i) = 0
+	* 		and if % of (decimal/i) = 0
+	* 		then divide (10/i) and (decimal/i)
+* loop again using new denominator instead of 10
+* if % never = 0 then end loop
+* display decimal in numerator - and display new numerator
+	* */
+
+
+
 }
