6c9299fc23e4dcd5a318338001e677993804ca16
Zachary Wilson <wilsonzachary634@gmail.com>
2018-03-10 19:09:54 -0700

    model and controller commit

---
+++ Calc360/app/src/main/java/edu/byui/cit/model/Academic.java
@@ -0,0 +1,19 @@
+package edu.byui.cit.model;
+
+public final class Academic {
+
+	private Academic() {
+
+	}
+
+	public static final class GPA {
+		private GPA() {
+
+		}
+		public static double calculateGPA() {
+			double gpa = 0;
+
+			return gpa;
+		}
+	}
+}
