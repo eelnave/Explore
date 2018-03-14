3d1b23193237a38adb304b25a9d3305f2a01eafa
Justin <ful14014@byui.edu>
2018-03-14 11:29:16 -0600

    Fixed the the Descriptor for the calculator

--- Calc360/app/src/main/java/edu/byui/cit/calc360/Descriptors.java
+++ Calc360/app/src/main/java/edu/byui/cit/calc360/Descriptors.java
@@ -23,7 +23,7 @@ final class Descriptors {
 				new CalcDescriptor(1012, R.string.bodyMassIndex, R.drawable.body_mass_index, BodyMassIndex.class),
 				new CalcDescriptor(1020, R.string.caloriesBurned, R.drawable.flame, CaloriesBurned.class),
 				new CalcDescriptor(1062, R.string.age, R.drawable.birthday_cake, Age.class),
-					new CalcDescriptor(1065, R.string.basalMetabolicRate,R.drawable.lightning,BasalMetabolicRate.class),
+					new CalcDescriptor(1067, R.string.basalMetabolicRate,R.drawable.lightning,BasalMetabolicRate.class),
 			}),
 			new GroupDescriptor(302, R.string.travel, R.drawable.folder_travel, new CalcDescriptor[]{
 				new CalcDescriptor(1003, R.string.FuelEconomy, R.drawable.fuel_gauge, FuelEconomy.class),
