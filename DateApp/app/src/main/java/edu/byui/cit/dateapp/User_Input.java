f26c464074b9f51e8ac94892be08505234e41869
victoriafisher <fis14005@byui.edu>
2018-03-15 12:04:30 -0600

    XML layouts

---
+++ DateApp/app/src/main/java/edu/byui/cit/dateapp/User_Input.java
@@ -0,0 +1,14 @@
+package edu.byui.cit.dateapp;
+
+/**
+ * Created by victoriafisher on 3/14/18.
+ */
+
+public class User_Input {
+
+    public String season;
+    public String money;
+    public String venue;
+    public boolean active;
+
+}
