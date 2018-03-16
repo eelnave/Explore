653d83417ff8f3a428ded4a11c5d1936dd49fa64
JoshK_000 <spr12011@byui.edu>
2018-03-16 10:52:40 -0600

    Getting the images to work

--- Calc360/app/src/main/java/edu/byui/cit/calc360/Descriptors.java
+++ Calc360/app/src/main/java/edu/byui/cit/calc360/Descriptors.java
@@ -113,7 +113,7 @@ final class Descriptors {
 				new CalcDescriptor(1060, R.string.musicDuration, R.drawable.music_notes, MusicDuration.class),
 			}),
 			new GroupDescriptor(314,R.string.academic, R.drawable.folder_academic, new CalcDescriptor[]{
-				new CalcDescriptor(1067, R.string.gpaCalc, R.drawable.GPA, GPA.class)
+				new CalcDescriptor(1067, R.string.gpaCalc, R.drawable.gpa, GPA.class)
 			}),
 			new GroupDescriptor(310, R.string.general, R.drawable.folder_general, new CalcDescriptor[]{
 				new CalcDescriptor(1032, R.string.unitConvert, R.drawable.unit_convert, UnitConvert.class),
