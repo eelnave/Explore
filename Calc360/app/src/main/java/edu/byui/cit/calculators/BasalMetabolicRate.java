4b4382d88fb6141a44cc2a0b2a7245329ade026c
Justin <ful14014@byui.edu>
2018-03-19 10:38:40 -0600

    Removed the commented out lines

--- Calc360/app/src/main/java/edu/byui/cit/calculators/BasalMetabolicRate.java
+++ Calc360/app/src/main/java/edu/byui/cit/calculators/BasalMetabolicRate.java
@@ -4,23 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-//import android.widget.RadioButton;
 
 import java.text.NumberFormat;
 
 import edu.byui.cit.calc360.CalcFragment;
 import edu.byui.cit.calc360.R;
-//import edu.byui.cit.text.ClickListener;
 import edu.byui.cit.text.ControlWrapper;
 import edu.byui.cit.text.EditDecimal;
 import edu.byui.cit.text.EditWrapper;
 import edu.byui.cit.text.RadioWrapper;
 import edu.byui.cit.text.TextWrapper;
-//import edu.byui.cit.units.Length;
-//import edu.byui.cit.units.Property;
-//import edu.byui.cit.units.Unit;
-//import edu.byui.cit.units.Mass;
-
 
 public final class BasalMetabolicRate extends CalcFragment {
 	private RadioWrapper radFemale;
@@ -29,7 +22,6 @@ public final class BasalMetabolicRate extends CalcFragment {
 	private final NumberFormat fmtrDec;
 
 	public BasalMetabolicRate() {
-		// Call the constructor in the parent class.
 		super();
 
 		fmtrDec = NumberFormat.getInstance();
@@ -41,7 +33,6 @@ public final class BasalMetabolicRate extends CalcFragment {
 	@Override
 	protected View createView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		// Inflate the layout for this calculator.
 		View view = inflater.inflate(R.layout.basal_metabolic_rate, container,
 				false);
 
@@ -50,7 +41,6 @@ public final class BasalMetabolicRate extends CalcFragment {
 		decAge = new EditDecimal(view, R.id.decAge, this);
 		txtBMR = new TextWrapper(view, R.id.txtBMR);
 
-		//RadioClick listener = new RadioClick();
 		radFemale = new RadioWrapper(view, R.id.radFemale, this);
 		new RadioWrapper(view, R.id.radMale, this);
 		radFemale.performClick();
@@ -63,33 +53,6 @@ public final class BasalMetabolicRate extends CalcFragment {
 		return view;
 	}
 
-
-	/*private final class RadioClick implements ClickListener {
-		@Override
-		public void clicked(View button) {
-			Property length = Length.getInstance();
-			Property mass = Mass.getInstance();
-			boolean selected = ((RadioButton)button).isChecked();
-			if (selected) {
-				Unit unitLength, unitMass;
-				switch (button.getId()) {
-					case R.id.radFemale:
-						unitLength = length.get(Length.inch);
-						unitMass = mass.get(Mass.pound);
-						break;
-					default:  // R.id.radMetric:
-						unitLength = length.get(Length.m);
-						unitMass = mass.get(Mass.kilogram);
-						break;
-				}
-				txtHeightUnit.setText(unitLength.getLocalName());
-				txtWeightUnit.setText(unitMass.getLocalName());
-			}
-			callCompute();
-		}
-	}*/
-
-
 	@Override
 	protected void compute() {
 		if (decHeight.notEmpty() && decWeight.notEmpty() && decAge.notEmpty()) {
@@ -97,12 +60,9 @@ public final class BasalMetabolicRate extends CalcFragment {
 			double weight = decWeight.getDec();
 			double age = decAge.getDec();
 			if (radFemale.isChecked()) {
-				//Female BMR calculation
 				double BMR = 10 * (weight / 2.2) + 6.25 * (height * 2.54) - 5 * (age) - 162;
 				txtBMR.setText(fmtrDec.format(BMR));
 			}else {
-
-				// Male BMR calculation
 				double BMR = 10 * (weight / 2.2) + 6.25 * (height * 2.54) - 5 * (age) + 4;
 				txtBMR.setText(fmtrDec.format(BMR));
 			}
