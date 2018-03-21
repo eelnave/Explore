a8094ee82c5794e3a793ae6dc724f7997605e2b1
bradkubie <kub13004@byui.edu>
2018-03-21 12:59:29 -0600

    Clean up

--- kindnessClient/app/src/main/java/edu/byui/cit/kindness/KindnessMap.java
+++ kindnessClient/app/src/main/java/edu/byui/cit/kindness/KindnessMap.java
@@ -2,23 +2,13 @@ package edu.byui.cit.kindness;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import org.xmlpull.v1.XmlPullParser;
-
-import java.util.zip.Inflater;
-
 
 public class KindnessMap extends FragmentActivity implements OnMapReadyCallback{
 
@@ -33,31 +23,6 @@ public class KindnessMap extends FragmentActivity implements OnMapReadyCallback{
 				.findFragmentById(R.id.map);
 		mapFragment.getMapAsync(this);
 	}
-//	View view;
-
-//	@Override
-//	public void onCreate(Bundle savedInstanceState){
-//		super.onCreate(savedInstanceState);
-//	}
-//
-//	@Override
-//	protected View createView(LayoutInflater inflater, ViewGroup container,
-//			Bundle savedInstState) {
-//		View view = inflater.inflate(R.layout.map, container, false);
-//		return view;
-//	}
-
-
-//	public void onViewCreated(View view, Bundle savedInstanceState){
-//		super.onViewCreated(view, savedInstanceState);
-//		MapView mapView = (MapView) view.findViewById(R.id.map);
-//		if(mapView != null){
-//			mapView.onCreate(null);
-//			mapView.onResume();
-//			mapView.getMapAsync(this);
-//		}
-//	}
-
 
 	@Override
 	public void onMapReady(GoogleMap googleMap) {
