75ed599fbe90b7a0d389b74179895d310fc49e5d
victoriafisher <fis14005@byui.edu>
2018-04-05 22:08:36 -0600

    added stuff to custom page

--- DateApp/app/src/main/java/edu/byui/cit/dateapp/DaterCreatorDBHelper.java
+++ DateApp/app/src/main/java/edu/byui/cit/dateapp/DaterCreatorDBHelper.java
@@ -34,6 +34,62 @@ public class DaterCreatorDBHelper extends SQLiteOpenHelper{
                 + DaterCreatorContract.DatesDone.DATE_INFO_ID + " INTEGER PRIMARY KEY AUTOINCREMENT);";
 
         sqLiteDatabase.execSQL(CREATE_USER_TABLE);
+
+
+        // do i need to add ''
+        // weather.. any?
+        // inside numbers?
+
+
+        /*
+        temp
+                + "("
+                + " \'\'" + ", "
+                + " \' \'" + ", "
+                + "1" + ", "
+                + "1" + ", "
+                + "\'any\'" + ", "
+                + "\'inside\'"
+                + "), "
+         */
+
+                                                    //dont know it this is right
+        final String INSERT_DATA = "INSERT INTO" +  DaterCreatorContract.DateInfo.TABLE_NAME + " ( "
+                + DaterCreatorContract.DateInfo.DATE_NAME_COLUMN + ", "
+                + DaterCreatorContract.DateInfo.DATE_DESCRIPTION_COLUMN + ", "
+                + DaterCreatorContract.DateInfo.DATE_COST_COLUMN_ + ", "
+                + DaterCreatorContract.DateInfo.DATE_ACTIVITY_LEVEL_COLUMN + ", "
+                + DaterCreatorContract.DateInfo.DATE_TEMP_COLUMN + ", "
+                + DaterCreatorContract.DateInfo.DATE_VENUE_COLUMN + ")"
+                + " VALUES "
+                + "("
+                + " \'Shooting star night Golf\'" + ", "
+                + " \'Grab some golf balls and glow sticks. Check you local DI for drivers. Head out to the sand dunes (or anywhere high) and break the glow stick open and cover golf balls in fluorescent light. See hoe far you can hit them and watch them soar \'" + ", "
+                + "2" + ", "
+                + "2" + ", "
+                + "\'warm\'" + ", "
+                + "\'outside\'"
+                + "), "
+                ////////////////////////////// end of that date
+                + "("
+                + " \'Netflix in Silence\'" + ", "
+                + " \'Watch a Netflix movie neither of you have seen and turn the sound off and see what kind of story you guys can come up with! You can spice it up by ordering take out or ice cream \'" + ", "
+                + "1" + ", "
+                + "1" + ", "
+                + "\'any\'" + ", "
+                + "\'inside\'"
+                + "), "
+                //////////////////////////end of that date
+                + "("
+                + " \'Arcade Game Night\'" + ", "
+                + " \'See how many ticket you can get for some little fun prizes\'" + ", "
+                + "2" + ", "
+                + "2" + ", "
+                + "\'any\'" + ", "
+                + "\'inside\'"
+                + "), "
+
+                + ");";
     }
 
     @Override
@@ -44,3 +100,6 @@ public class DaterCreatorDBHelper extends SQLiteOpenHelper{
 
     }
 }
+
+
+// String Insert
