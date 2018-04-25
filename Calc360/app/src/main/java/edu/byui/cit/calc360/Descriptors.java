968e4b782db1c3c4b089d46f3a87543736979265
BarzeeRex <barzeer@byui.edu>
2018-04-25 13:51:06 -0600

    Add HabitCost calculator

--- Calc360/app/src/main/java/edu/byui/cit/calc360/Descriptors.java
+++ Calc360/app/src/main/java/edu/byui/cit/calc360/Descriptors.java
@@ -42,9 +42,9 @@ final class Descriptors {
 				new CalcDescriptor(1005, R.string.loan, R.drawable.loan, Loan.class),
 				new CalcDescriptor(1016, R.string.roi, R.drawable.roi, ReturnOnInvestment.class),
 				new CalcDescriptor(1022, R.string.laborCost, R.drawable.labor_cost, LaborCost.class),
+				new CalcDescriptor(1074, R.string.habitCost, R.drawable.lightning, HabitCost.class),
 				new CalcDescriptor(1018, R.string.tithing, R.drawable.coins, Tithing.class),
 				new CalcDescriptor(1066, R.string.stream, R.drawable.netflix, StreamingCost.class),
-				new CalcDescriptor(1072, R.string.habitCost, R.drawable.lightning, HabitCost.class),
 			}),
 			new GroupDescriptor(311,R.string.cooking, R.drawable.folder_cooking, new CalcDescriptor[]{
 				new CalcDescriptor(1052, R.string.recipeMult, R.drawable.recipe_book, RecipeMultiplier.class),
