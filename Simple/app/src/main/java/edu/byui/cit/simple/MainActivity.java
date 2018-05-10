825548ce708aeac7aa0ef106d3c2d6b5eeadd50a
BarzeeRex <barzeer@byui.edu>
2018-05-10 08:58:26 -0600

    Add functionality to the Simple app example for students

--- Simple/app/src/main/java/edu/byui/cit/simple/MainActivity.java
+++ Simple/app/src/main/java/edu/byui/cit/simple/MainActivity.java
@@ -8,45 +8,77 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+	private EditText txtOne, txtTwo;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+		txtOne = findViewById(R.id.editText);
+		txtTwo = findViewById(R.id.editText2);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+		Button btnCompute = findViewById(R.id.button);
+		Button btnClear = findViewById(R.id.button2);
+		btnCompute.setOnClickListener(new ComputeHandler());
+		btnClear.setOnClickListener(new ClearHandler());
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+		FloatingActionButton fab = findViewById(R.id.fab);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Snackbar.make(view, "Replace with your own action",
+						Snackbar.LENGTH_LONG)
+						.setAction("Action", null).show();
+			}
+		});
+	}
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items
+		// to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
 
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+
+	private final class ComputeHandler implements View.OnClickListener {
+		@Override
+		public void onClick(View button) {
+			double one = Double.parseDouble(txtOne.getText().toString());
+			txtTwo.setText(Double.toString(one));
+		}
+	}
+
+	private final class ClearHandler implements View.OnClickListener {
+		@Override
+		public void onClick(View button) {
+			txtOne.getText().clear();
+			txtTwo.getText().clear();
+			txtOne.requestFocus();
+		}
+	}
 }
