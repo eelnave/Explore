b5e52de6995dc262ac3a52225e4f8a1cacdaf9cd
50mm3r <som13002@byui.edu>
2018-05-16 11:12:09 -0600

    Java Logic

--- FirstApp/app/src/main/java/com/byuicit360/example/firstapp/MainActivity.java
+++ FirstApp/app/src/main/java/com/byuicit360/example/firstapp/MainActivity.java
@@ -5,11 +5,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtOne, txtTwo;
+    private TextView txtFour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +28,56 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        txtOne = findViewById(R.id.editText);
+        txtTwo = findViewById(R.id.editText2);
+        txtFour = findViewById(R.id.textView4);
+
+        Button btnClear = findViewById(R.id.button2);
+        btnClear.setOnClickListener(new MowTheGrass());
+
+        txtTwo.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                NumberFormat formatter = NumberFormat.getInstance();
+                String input = txtOne.getText().toString();
+                String input2 = txtTwo.getText().toString();
+                try {
+                    Number number = formatter.parse(input);
+                    Number number2 = formatter.parse(input2);
+                    double one = number.doubleValue();
+                    double two = number2.doubleValue();
+                    double result = Math.pow(one, two);
+                    txtFour.setText(formatter.format(result));
+                }
+                catch (Exception ex) {
+
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
+
     }
+
+    private final class MowTheGrass implements View.OnClickListener {
+        @Override
+
+        public void onClick(View button2) {
+            txtOne.getText().clear();
+            txtTwo.getText().clear();
+            txtOne.requestFocus();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
