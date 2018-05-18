8c1c267c6b26b618758c11c036492dd0e5597bc4
kar15003@byui.edu <Ruth5184998672>
2018-05-18 10:20:26 -0600

    ruth's edition

--- Simple/app/src/main/java/edu/byui/cit/simple/MainActivity.java
+++ Simple/app/src/main/java/edu/byui/cit/simple/MainActivity.java
@@ -1,84 +1,96 @@
 package edu.byui.cit.simple;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-	private EditText txtOne, txtTwo;
+
+	private EditText num1;
+	private EditText num2;
+	private TextView result;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
-		Toolbar toolbar = findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
 
-		txtOne = findViewById(R.id.editText);
-		txtTwo = findViewById(R.id.editText2);
 
-		Button btnCompute = findViewById(R.id.button);
-		Button btnClear = findViewById(R.id.button2);
-		btnCompute.setOnClickListener(new ComputeHandler());
-		btnClear.setOnClickListener(new ClearHandler());
+		num1 =findViewById(R.id.etNum1);
+		num2 =findViewById(R.id.etNum2);
+		final Button btnClear =findViewById(R.id.btnClear);
+		result = findViewById(R.id.tvAnswer);
 
-		FloatingActionButton fab = findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
+		btnClear.setOnClickListener(new View.OnClickListener() {
+
 			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action",
-						Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+			public void onClick(View v) {
+				num1.getText().clear();
+				num2.getText().clear();
+				result.setText("");
+				num1.requestFocus();
+
+
 			}
 		});
+
+		num1.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				    try {
+						double number1 = Double.parseDouble(num1.getText().toString());
+						double number2 = Double.parseDouble(num2.getText().toString());
+						double sum = Math.pow(number1,number2);
+						result.setText(String.valueOf(sum));
+					}
+					catch(Exception ex) {
+			        	//Do nothing
+					}
+
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+
+			}
+		});
+        num2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    double number1 = Double.parseDouble(num1.getText().toString());
+                    double number2 = Double.parseDouble(num2.getText().toString());
+                    double sum = Math.pow(number1,number2);
+                    result.setText(String.valueOf(sum));
+                }
+                catch(Exception ex) {
+                    //Do nothing
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items
-		// to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
-
-
-	private final class ComputeHandler implements View.OnClickListener {
-		@Override
-		public void onClick(View button) {
-			double one = Double.parseDouble(txtOne.getText().toString());
-			txtTwo.setText(Double.toString(one));
-		}
-	}
-
-	private final class ClearHandler implements View.OnClickListener {
-		@Override
-		public void onClick(View button) {
-			txtOne.getText().clear();
-			txtTwo.getText().clear();
-			txtOne.requestFocus();
-		}
-	}
 }
