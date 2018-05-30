348408744afb4f6658ae0e8ed32ae341e87c083a
Colin Fotheringham <colinfthrnghm@outlook.com>
2018-05-29 18:07:51 -0600

    privacy page

---
+++ kindnessClient/app/src/main/java/edu/byui/cit/kindness/PrivateFragment.java
@@ -0,0 +1,30 @@
+package edu.byui.cit.kindness;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public final class PrivateFragment extends CITFragment {
+    @Override
+    protected String getTitle() {
+        return "Privacy";
+    }
+
+    @Override
+
+    protected View createView(LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.privacy_page, container, false);
+
+        view.findViewById(R.id.btnToMap2).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        getActivity().onBackPressed();
+                    }
+                });
+
+        return view;
+    }
+}
