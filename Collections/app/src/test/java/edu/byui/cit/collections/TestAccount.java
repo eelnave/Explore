5e01d81a34de2aa00af1006acc1d5ee3b337f7bb
BarzeeRex <barzeer@byui.edu>
2018-05-31 17:22:43 +0100

    Attempt to stop Android Studio from overwriting code style files

---
+++ Collections/app/src/test/java/edu/byui/cit/collections/TestAccount.java
@@ -0,0 +1,14 @@
+package edu.byui.cit.collections;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class TestAccount {
+    @Test
+    public void sampleFailingTest() {
+        int i = 23;
+        assertEquals(1, i);
+    }
+}
