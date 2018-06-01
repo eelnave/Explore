e2c1924ec76d4f0213fe5cae2f2685aae19cada6
parkergoldsberry <gol13002@byui.edu>
2018-06-01 11:23:42 -0600

    Adding Java code to custom goal activity.

--- Record/app/src/main/java/edu/byui/cit/record/add_customgoal.java
+++ Record/app/src/main/java/edu/byui/cit/record/add_customgoal.java
@@ -2,12 +2,34 @@ package edu.byui.cit.record;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class add_customgoal extends AppCompatActivity {
 
+    private EditText txtGoalName, txtCompletiondate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_customgoal);
+
+        //set up text boxes to be read
+        txtGoalName = findViewById(R.id.goalName);
+
+        //set up button to be clicked
+        Button submitButton = findViewById(R.id.submitFormButton);
+        submitButton.setOnClickListener(new NewGenericGoal());
+    }
+
+    private class NewGenericGoal implements View.OnClickListener {
+        @Override
+        public void onClick(View submitButton){
+            //put what happens when the RECORD! button is clicked.
+            //collect data from the form.
+
+            //make a new row in the goal database with the data.
+
+        }
     }
 }
