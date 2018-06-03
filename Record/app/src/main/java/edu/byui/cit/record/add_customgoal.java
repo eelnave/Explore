1d4bd6734ed873d53a83cbe61beaa623efb81d0a
parkergoldsberry <gol13002@byui.edu>
2018-06-02 18:58:13 -0600

    Worked on Java code for new generic goal screen. Still need to add code to insert a new row in the database once database objects are ready to use.

--- Record/app/src/main/java/edu/byui/cit/record/add_customgoal.java
+++ Record/app/src/main/java/edu/byui/cit/record/add_customgoal.java
@@ -4,9 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class add_customgoal extends AppCompatActivity {
+
+    //global integer to hold the current radio button selection.
+    private static int buttonSelection = 0;
+    private static boolean checkedOngoing = false;
 
     private EditText txtGoalName, txtCompletiondate;
     @Override
@@ -16,10 +25,46 @@ public class add_customgoal extends AppCompatActivity {
 
         //set up text boxes to be read
         txtGoalName = findViewById(R.id.goalName);
+        txtCompletiondate = findViewById(R.id.finishDate);
 
         //set up button to be clicked
         Button submitButton = findViewById(R.id.submitFormButton);
         submitButton.setOnClickListener(new NewGenericGoal());
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        //check which button was clicked and assign the corresponding value to the buttonSelection variable
+        //1=daily, 2=weekly, 3=morning and night
+        switch (view.getId()){
+            case R.id.daily:
+                if (checked){
+                    buttonSelection = 1;
+                }
+            case R.id.weekly:
+                if (checked){
+                    buttonSelection = 2;
+                }
+            case R.id.morningAndNight:
+                if (checked){
+                    buttonSelection = 3;
+                }
+        }
+    }
+
+    public void onCheckBoxClicked(View view) {
+        boolean isChecked = ((CheckBox) view).isChecked();
+
+        switch(view.getId()){
+            case R.id.checkBoxOngoing:
+                if (isChecked){
+                    checkedOngoing = true;
+                }
+                else {
+                    checkedOngoing = false;
+                }
+        }
     }
 
     private class NewGenericGoal implements View.OnClickListener {
@@ -27,9 +72,15 @@ public class add_customgoal extends AppCompatActivity {
         public void onClick(View submitButton){
             //put what happens when the RECORD! button is clicked.
             //collect data from the form.
+            String goalName = txtGoalName.toString();
+
+            String parseDate = txtCompletiondate.toString();
+            SimpleDateFormat finishDate = new SimpleDateFormat(parseDate);
+
+            //--state of check boxes and radio buttons are stored in global variables already defined.--
 
             //make a new row in the goal database with the data.
-
+            //TODO: once database objects are ready, insert all of this data into a new row.
         }
     }
 }
