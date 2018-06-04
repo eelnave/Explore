4fb0af9ba56ccd3a90ccab2f55910bfeeaa0eab9
parkergoldsberry <gol13002@byui.edu>
2018-06-04 11:37:37 -0600

    Added a plus button that starts the new goal activity.

--- Record/app/src/main/java/edu/byui/cit/record/MainActivity.java
+++ Record/app/src/main/java/edu/byui/cit/record/MainActivity.java
@@ -1,8 +1,11 @@
 package edu.byui.cit.record;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
+
+		FloatingActionButton fab = findViewById(R.id.newGoalFAB);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				//make the jump to the new goal screen
+				Intent intent = new Intent(MainActivity.this, add_customgoal.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
--- Record/app/src/main/java/edu/byui/cit/record/add_customgoal.java
+++ Record/app/src/main/java/edu/byui/cit/record/add_customgoal.java
@@ -3,10 +3,12 @@ package edu.byui.cit.record;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,6 +24,15 @@ public class add_customgoal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_customgoal);
+
+        //populate the spinner with the frequency choices
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.frequencyChoices, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
         //set up text boxes to be read
         txtGoalName = findViewById(R.id.goalName);
