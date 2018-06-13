cdbcf9960658eb9892bb4e8f1dac499f6db0045a
parkergoldsberry <gol13002@byui.edu>
2018-06-13 11:02:12 -0600

    UNDOING BAD JUJU -- CRASHING FOR NO REASON

--- Record/app/src/main/java/edu/byui/cit/record/MainActivity.java
+++ Record/app/src/main/java/edu/byui/cit/record/MainActivity.java
@@ -1,11 +1,15 @@
 package edu.byui.cit.record;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,5 +29,16 @@ public class MainActivity extends AppCompatActivity {
 				startActivity(intent);
 			}
 		});
+		//Daily notification time, intent, and alarm manager
+		Calendar calendar = Calendar.getInstance();
+//    calendar.set(Calendar.HOUR_OF_DAY, 18);
+//    calendar.set(Calendar.MINUTE, 30);
+		calendar.add(Calendar.SECOND, 5);
+
+		Intent intent = new Intent(getApplicationContext(), NotificationReceiver.class);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+		AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+		alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
 	}
 }
---
+++ Record/app/src/main/java/edu/byui/cit/record/NotificationReceiver.java
@@ -0,0 +1,36 @@
+package edu.byui.cit.record;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+public class NotificationReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Intent repeating_intent = new Intent(context, RepeatingNotification.class);
+        repeating_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 100, repeating_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "NotificationID")
+                .setSmallIcon(android.R.drawable.arrow_up_float)
+                .setContentTitle("Your Goal")
+                .setContentText("Did you complete it?")
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        notificationManager.notify(100, builder.build());
+    }
+
+
+
+}
