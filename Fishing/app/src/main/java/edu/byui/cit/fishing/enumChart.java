3275c339fefd9acb18b4da704da32c567bd93096
bar12079 <bar12079@byui.edu>
2018-06-15 10:57:19 -0600

    commit

---
+++ Fishing/app/src/main/java/edu/byui/cit/fishing/enumChart.java
@@ -0,0 +1,48 @@
+package edu.byui.cit.fishing;
+
+public class enumChart {
+
+    public enum bodysOfWater
+    {
+        UpperHenrysFork,
+        LowerHenrysFork,
+        SnakeRiver,
+        TetonRiver,
+        WarmRiver,
+        FallRiver,
+        AshtonRes,
+        IslandParkRes,
+        HenrysLake,
+        RirieRes,
+        WakesideLake,
+        MudLake,
+        PalisadesRes
+    }
+
+    public enum month{
+        jan,
+        feb,
+        mar,
+        apr,
+        may,
+        jun,
+        jul,
+        aug,
+        sep,
+        oct,
+        nov,
+        dec
+    }
+    public enum species {
+        rainbow,
+        cutthroat,
+        brown,
+        brook,
+        lakeTrout,
+        kokanee,
+        smallMouth,
+        largeMouth
+
+    }
+}
+
