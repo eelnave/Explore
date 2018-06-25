8024b1cc97c5cca01f8d3111ebdb5d135cf0e736
Jeanina Lao <lao13001@byui.edu>
2018-06-25 10:10:03 -0600

    Tried to fix Help

--- Maintenance/app/src/main/java/edu/byui/cit/maintenance/Help.java
+++ Maintenance/app/src/main/java/edu/byui/cit/maintenance/Help.java
@@ -17,9 +17,9 @@ public class Help extends CITFragment {
     @Override
     protected View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.help, container,  false);
-        //String helpTitle = R.string.
+        String helpTitle = "How to use the Maintenance App";
         TextView title = view.findViewById(R.id.textTitle);
-        title.setText(R.string.helpTextTitle);
+        title.setText(helpTitle);
         return null;
     }
 }
