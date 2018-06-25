ce8c9b0769957df949b845776d3a99f1135d0dc1
ammoncates <35459382+ammoncates@users.noreply.github.com>
2018-06-25 13:43:52 -0600

    Finished  Upper and Lower Henry's for all months. Output will currently show in tvTest textview.

--- Fishing/app/src/main/java/edu/byui/cit/fishing/Choices.java
+++ Fishing/app/src/main/java/edu/byui/cit/fishing/Choices.java
@@ -45,149 +45,149 @@ public class Choices extends MainActivity {
         SelectedWater = selectedWater;
     }
 
-    public enum water
-    {
-        UpperHenry,
-        LowerHenry,
-        SnakeRiver,
-        TetonRiver,
-        WarmRiver,
-        FallRiver,
-       /* AshtonRes,
-        IslandParkRes,
-        HenrysLake,
-        RirieRes,
-        WakesideLake,
-        MudLake,
-        PalisadesRes */
-    }
-
-    public enum month{
-        January,
-        February,
-        March,
-        April,
-        May,
-        June,
-        July,
-        August,
-        September,
-        October,
-        November,
-        December
-    }
-    public enum species {
-        Rainbow,
-        Cutthroat,
-        Brown,
-        Brook
-        /* LakeTrout,
-        Kokanee,
-        SmallMouth,
-        LargeMouth */
-
-    }
-
-   // convert species strings to enums
-    public Choices.species speciesStringToEnum(String selectedSpecies){
-
-        Choices.species speciesEnum = species.Rainbow;
-        switch (selectedSpecies) {
-            case "Rainbow Trout":
-                speciesEnum = species.Rainbow;
-                break;
-            case "Cutthroat Trout":
-                speciesEnum = species.Cutthroat;
-                break;
-            case "Brown Trout":
-                speciesEnum = species.Brown;
-                break;
-            case "Brook trout":
-                speciesEnum = species.Brook;
-                break;
-            default:
-                break;
-        }
-        return speciesEnum;
-    }
-
-    //convert water strings to enums
-    public Choices.water waterStringToEnum(String selectedWater){
-
-        Choices.water waterEnum = water.UpperHenry;
-        switch (selectedWater) {
-            case "Upper Henry's Fork":
-                waterEnum = water.UpperHenry;
-                break;
-            case "Lower Henry's Fork":
-                waterEnum = water.LowerHenry;
-                break;
-            case "Snake River":
-                waterEnum = water.SnakeRiver;
-                break;
-            case "Teton River":
-                waterEnum = water.TetonRiver;
-                break;
-            case "Warm River":
-                waterEnum = water.WarmRiver;
-                break;
-            case "Fall River":
-                waterEnum = water.FallRiver;
-                break;
-            default:
-
-                break;
-        }
-        return waterEnum;
-    }
-
-    //convert water strings to enums
-    public Choices.month monthStringToEnum(String selectedMonth){
-
-        Choices.month monthEnum = month.January;
-        switch (selectedMonth) {
-            case "January":
-                monthEnum = month.January;
-                break;
-            case "February":
-                monthEnum = month.February;
-                break;
-            case "March":
-                monthEnum = month.March;
-                break;
-            case "April":
-                monthEnum = month.April;
-                break;
-            case "May":
-                monthEnum = month.May;
-                break;
-            case "June":
-                monthEnum = month.June;
-                break;
-            case "July":
-                monthEnum = month.July;
-                break;
-            case "August":
-                monthEnum = month.August;
-                break;
-            case "September":
-                monthEnum = month.September;
-                break;
-            case "October":
-                monthEnum = month.October;
-                break;
-            case "November":
-                monthEnum = month.November;
-                break;
-            case "December":
-                monthEnum = month.December;
-                break;
-            default:
-
-                break;
-        }
-        return monthEnum;
-    }
+//    public enum water
+//    {
+//        UpperHenry,
+//        LowerHenry,
+//        SnakeRiver,
+//        TetonRiver,
+//        WarmRiver,
+//        FallRiver,
+//       /* AshtonRes,
+//        IslandParkRes,
+//        HenrysLake,
+//        RirieRes,
+//        WakesideLake,
+//        MudLake,
+//        PalisadesRes */
+//    }
+//
+//    public enum month{
+//        January,
+//        February,
+//        March,
+//        April,
+//        May,
+//        June,
+//        July,
+//        August,
+//        September,
+//        October,
+//        November,
+//        December
+//    }
+//    public enum species {
+//        Rainbow,
+//        Cutthroat,
+//        Brown,
+//        Brook
+//        /* LakeTrout,
+//        Kokanee,
+//        SmallMouth,
+//        LargeMouth */
+//
+//    }
+//
+//   // convert species strings to enums
+//    public Choices.species speciesStringToEnum(String selectedSpecies){
+//
+//        Choices.species speciesEnum = species.Rainbow;
+//        switch (selectedSpecies) {
+//            case "Rainbow Trout":
+//                speciesEnum = species.Rainbow;
+//                break;
+//            case "Cutthroat Trout":
+//                speciesEnum = species.Cutthroat;
+//                break;
+//            case "Brown Trout":
+//                speciesEnum = species.Brown;
+//                break;
+//            case "Brook trout":
+//                speciesEnum = species.Brook;
+//                break;
+//            default:
+//                break;
+//        }
+//        return speciesEnum;
+//    }
+//
+//    //convert water strings to enums
+//    public Choices.water waterStringToEnum(String selectedWater){
+//
+//        Choices.water waterEnum = water.UpperHenry;
+//        switch (selectedWater) {
+//            case "Upper Henry's Fork":
+//                waterEnum = water.UpperHenry;
+//                break;
+//            case "Lower Henry's Fork":
+//                waterEnum = water.LowerHenry;
+//                break;
+//            case "Snake River":
+//                waterEnum = water.SnakeRiver;
+//                break;
+//            case "Teton River":
+//                waterEnum = water.TetonRiver;
+//                break;
+//            case "Warm River":
+//                waterEnum = water.WarmRiver;
+//                break;
+//            case "Fall River":
+//                waterEnum = water.FallRiver;
+//                break;
+//            default:
+//
+//                break;
+//        }
+//        return waterEnum;
+//    }
+//
+//    //convert water strings to enums
+//    public Choices.month monthStringToEnum(String selectedMonth){
+//
+//        Choices.month monthEnum = month.January;
+//        switch (selectedMonth) {
+//            case "January":
+//                monthEnum = month.January;
+//                break;
+//            case "February":
+//                monthEnum = month.February;
+//                break;
+//            case "March":
+//                monthEnum = month.March;
+//                break;
+//            case "April":
+//                monthEnum = month.April;
+//                break;
+//            case "May":
+//                monthEnum = month.May;
+//                break;
+//            case "June":
+//                monthEnum = month.June;
+//                break;
+//            case "July":
+//                monthEnum = month.July;
+//                break;
+//            case "August":
+//                monthEnum = month.August;
+//                break;
+//            case "September":
+//                monthEnum = month.September;
+//                break;
+//            case "October":
+//                monthEnum = month.October;
+//                break;
+//            case "November":
+//                monthEnum = month.November;
+//                break;
+//            case "December":
+//                monthEnum = month.December;
+//                break;
+//            default:
+//
+//                break;
+//        }
+//        return monthEnum;
+//    }
 
 
     // compute fly output based on water,species, and month
@@ -197,19 +197,118 @@ public class Choices extends MainActivity {
         String reccomendedFly = " holder";
 
 
-    // sample case
+    // Upper Henry's selected
         switch(selectedWater){
             case "Upper Henry's Fork":
 
+                //Filter by month
             switch (selectedMonth){
                 case "January":
                     reccomendedFly = res.getString(R.string.upperHenrysJan);
-                    System.out.println(reccomendedFly);
-                    break;
+                    return reccomendedFly;
 
+                case "February":
+                    reccomendedFly = res.getString(R.string.upperHenrysFeb);
+                    return reccomendedFly;
 
+                case "March":
+                    reccomendedFly = res.getString(R.string.upperHenrysMar);
+                    return reccomendedFly;
 
+                case "April":
+                    reccomendedFly = res.getString(R.string.upperHenrysApr);
+                    return reccomendedFly;
+
+                case "May":
+                    reccomendedFly = res.getString(R.string.upperHenrysMay);
+                    return reccomendedFly;
+
+                case "June":
+                    reccomendedFly = res.getString(R.string.upperHenrysJun);
+                    return reccomendedFly;
+
+                case "July":
+                    reccomendedFly = res.getString(R.string.upperHenrysJul);
+                    return reccomendedFly;
+
+                case "August":
+                    reccomendedFly = res.getString(R.string.upperHenrysAug);
+                    return reccomendedFly;
+
+                case "September":
+                    reccomendedFly = res.getString(R.string.upperHenrysSep);
+                    return reccomendedFly;
+
+                case "October":
+                    reccomendedFly = res.getString(R.string.upperHenrysOct);
+                    return reccomendedFly;
+
+                case "November":
+                    reccomendedFly = res.getString(R.string.upperHenrysNov);
+                    return reccomendedFly;
+
+                case "December":
+                    reccomendedFly = res.getString(R.string.upperHenrysDec);
+                    return reccomendedFly;
+
+                default: throw new IllegalArgumentException("Invalid Combination");
             }
+
+            case "Lower Henry's Fork":
+
+                switch (selectedMonth){
+                    case "January":
+                        reccomendedFly = res.getString(R.string.lowerHenrysJan);
+                        return reccomendedFly;
+
+                    case "February":
+                        reccomendedFly = res.getString(R.string.lowerHenrysFeb);
+                        return reccomendedFly;
+
+                    case "March":
+                        reccomendedFly = res.getString(R.string.lowerHenrysMar);
+                        return reccomendedFly;
+
+                    case "April":
+                        reccomendedFly = res.getString(R.string.lowerHenrysApr);
+                        return reccomendedFly;
+
+                    case "May":
+                        reccomendedFly = res.getString(R.string.lowerHenrysMay);
+                        return reccomendedFly;
+
+                    case "June":
+                        reccomendedFly = res.getString(R.string.lowerHenrysJun);
+                        return reccomendedFly;
+
+                    case "July":
+                        reccomendedFly = res.getString(R.string.lowerHenrysJul);
+                        return reccomendedFly;
+
+                    case "August":
+                        reccomendedFly = res.getString(R.string.lowerHenrysAug);
+                        return reccomendedFly;
+
+                    case "September":
+                        reccomendedFly = res.getString(R.string.lowerHenrysSep);
+                        return reccomendedFly;
+
+                    case "October":
+                        reccomendedFly = res.getString(R.string.lowerHenrysOct);
+                        return reccomendedFly;
+
+                    case "November":
+                        // WET vs regular ???
+                        reccomendedFly = res.getString(R.string.lowerHenrysNovWet);
+                        return reccomendedFly;
+
+                    case "December":
+                        reccomendedFly = res.getString(R.string.upperHenrysDec);
+                        return reccomendedFly;
+
+                    default: throw new IllegalArgumentException("Invalid Combination");
+                }
+
             default: break;
             }
             return reccomendedFly;
