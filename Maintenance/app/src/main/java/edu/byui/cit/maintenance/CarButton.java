4baa8d6820f8ae3fdeb0a2bce4cd939d1bf4cf4b
Jeanina Lao <lao13001@byui.edu>
2018-06-29 10:47:31 -0600

    Trying to Add a Car button

---
+++ Maintenance/app/src/main/java/edu/byui/cit/maintenance/CarButton.java
@@ -0,0 +1,12 @@
+package edu.byui.cit.maintenance;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.widget.Button;
+
+
+public class CarButton extends AppCompatButton {
+	public CarButton(Context context) {
+		super(context);
+	}
+}
