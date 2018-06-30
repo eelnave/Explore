546cb95c76b9a95b674374cd105d82d74efc86e3
benjamin_poore <924Cosmo17!>
2018-06-30 13:52:09 -0600

    1.1MADE CHANGES IN ChooseVehicle.java 1.2 Added button and setOnClickListener for v1 to change to MaintenanceFrag 2. Cleaned up unnecessary code in MainActivity and ChooseVehicle 3. Added Oil_Frag layout 4. Created setOnClickListener for Oil to change to instance of OilFrag 5. Added content for the oil_frag page 6. Created two EditTexts, the first looking for input from user on mileage on car at last oil change and the second to then calculate (at the press of a button) when you need to get another oil change based on the mileage calculation.

--- Maintenance/app/src/main/java/edu/byui/cit/maintenance/OilFrag.java
+++ Maintenance/app/src/main/java/edu/byui/cit/maintenance/OilFrag.java
@@ -5,23 +5,70 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 import edu.byui.cit.widget.CITFragment;
 
 
 public class OilFrag extends CITFragment {
 
+	private EditText editText_LOC, editText_NOC;
+
+	// create Spinner object
+	// Spinner spinner;
+	// ArrayAdapter<CharSequence> adapter;
+
+
 	@Override
 	protected View createView(LayoutInflater inflater,
 			ViewGroup container, Bundle savedInstState) {
 
 		View view = inflater.inflate(R.layout.oil_frag, container, false);
+
+		// find editTexts by ID
+		editText_LOC = view.findViewById(R.id.editText_LOC);
+		editText_NOC = view.findViewById(R.id.editText_NOC);
+		// find button by id
+		Button b_compute = view.findViewById(R.id.b_compute);
+		// setOnClickListener
+		b_compute.setOnClickListener(new computeNOC());
+
+		// find spinner by ID
+		//spinner = view.findViewById(R.id.spinner);
+		//adapter = ArrayAdapter.createFromResource(this,R.array.oil_change_frequency,android.R.layout.simple_spinner_item);
+
+
 		return view;
 	}
 
 	@Override
 	protected String getTitle() {
 		return null;
+	}
+
+	private class computeNOC implements View.OnClickListener {
+		@Override
+		public void onClick(View b_compute) {
+			NumberFormat formatter = NumberFormat.getInstance();
+			String input = editText_LOC.getText().toString();
+			try {
+				Number number = formatter.parse(input);
+				double one = number.doubleValue();
+				double result = one + 3000;
+				editText_NOC.setText(formatter.format(result));
+			}
+			catch (Exception ex) {
+				// Don't do anything for this exception
+			}
+
+		}
+
+
 	}
 }
