489b36f6c513760e0a06b3b585454d0a086e9e82
Jeanina Lao <lao13001@byui.edu>
2018-07-07 23:00:28 -0600

    Studying Room

---
+++ Maintenance/app/src/main/java/edu/byui/cit/model/MaintenanceDAO.java
@@ -0,0 +1,36 @@
+package edu.byui.cit.model;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+
+
+@Dao
+public interface MaintenanceDAO {
+	@Query("SELECT * FROM vehicle")
+	List<Vehicle> getAll();
+
+	@Query("SELECT * FROM vehicle WHERE vehicleID IN (:vehicleIDs)")
+	List<Vehicle> getAllByID(int[] vehicleIDs);
+
+	@Query("SELECT * FROM vehicle WHERE vehicleID = :id")
+	Vehicle getByID(int id);
+
+	@Query("SELECT * FROM vehicle WHERE vin = :vin")
+	Vehicle getByVIN(String vin);
+
+	@Insert
+	long insert(Vehicle vehicle);
+
+	@Insert
+	void insertAll(Vehicle... vehicles);
+
+	@Delete
+	int delete(Vehicle vehicle);
+
+}
+
