67d949b821213b9c7a4b4ae1f2a913da68b88e21
benjamin_poore <924Cosmo17!>
2018-07-14 22:10:11 -0600

    Week 12                                     1. Troubleshooted different instances and how that impacts the app functionality if the database gets changed. Learned how to clear                                       data on application to resolve issue! Really big discovery for this!
                                        2. Added strName.requestFocus(); and updated the AddVehicle class to function as intended with the additional strName field.
    
                                        3. Edited BatteryFrag class / layout
    
                                        4. Researched how to add a click listener to enable clicking enter instead of add, but then realized that for a mobile device that                                        would be irrelevant.
    
                                        5. Changed functionality so when you click to add a new vehicle on the AddVehicle page it takes you to the choose_vehicle frag
    
                                        6. Changed functionality so when you click to add a new vehicle on the AddVehicle page it also clears the user input for all text                                         fields.

--- Maintenance/app/src/main/java/edu/byui/cit/maintenance/AddVehicle.java
+++ Maintenance/app/src/main/java/edu/byui/cit/maintenance/AddVehicle.java
@@ -2,10 +2,13 @@ package edu.byui.cit.maintenance;
 
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnKeyListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import edu.byui.cit.model.AppDatabase;
 import edu.byui.cit.model.Vehicle;
@@ -45,6 +48,38 @@ public class AddVehicle extends CITFragment {
 		new ButtonWrapper(view, R.id.btnAdd, new AddHandler());
 		new ButtonWrapper(view, R.id.btnReset, new ResetHandler());
 
+		// add OnKeyListener to EditString
+		//view = (EditString) findViewById(R.id.color);
+		view.setOnKeyListener(new OnKeyListener()
+		{
+			public boolean onKey(View v, int keyCode, KeyEvent event)
+			{
+				if (event.getAction() == KeyEvent.ACTION_DOWN)
+				{
+					switch (keyCode)
+					{
+						case KeyEvent.KEYCODE_DPAD_CENTER:
+						case KeyEvent.KEYCODE_ENTER:
+							new AddHandler();
+							return true;
+						default:
+							break;
+					}
+				}
+				return false;
+			}
+		});
+
+
+
+
+
+
+
+
+
+
+
 		return view;
 	}
 
@@ -54,9 +89,17 @@ public class AddVehicle extends CITFragment {
 		return getActivity().getString(R.string.addVehicle);
 	}
 
+
+
+
+
+
 	private final class AddHandler implements ClickListener {
+		private EditText addCourseText;
+
 		@Override
 		public void clicked(WidgetWrapper source) {
+
 /**
 			new AsyncTask<Void, Void, Void>() {
 				@Override
@@ -110,36 +153,35 @@ public class AddVehicle extends CITFragment {
 			vehicleDAO = db.getVehicleDAO();
             vehicleDAO.insert(newVehicle);
 
-
-			// prepend "view" to view.findViewById(R.id.v1); because you are outside of MainActivity
-			//btnAdd = view.findViewById(R.id.btnAdd);
-			//btnAdd.setOnClickListener(new View.OnClickListener() {
-				//@Override
-				//public void onClick(View view) {
+            //change to choose_vehicle frag
 					if (chooseVehicle == null || chooseVehicle.isDetached()) {
 						chooseVehicle = new ChooseVehicle();
 					}
 
-					//switch to fragment fragAct (for viewing vehicle details)
+					//clear all fields
+					strName.clear();
+					decYear.clear();
+					strMake.clear();
+					strModel.clear();
+					strVin.clear();
+					strColor.clear();
+					decYear.requestFocus();
+
+					//switch to fragment chooseVehicle (to see newly added vehicle)
 					switchFragment(chooseVehicle);
 				}
-			//});
-
 
 	//added switchFragment for Add onClickListener
 	public void switchFragment(CITFragment fragment) {
 		// Replace whatever is in the fragContainer view with
 		// fragment, and add the transaction to the back stack so
 		// that the user can navigate back.
+
 		FragmentTransaction trans = getFragmentManager().beginTransaction();
 		trans.replace(R.id.fragContainer, fragment, "thing");
 		trans.addToBackStack(null);
 		trans.commit();
 	}
-
-	// call ResetHandler to reset all fields
-	//	ResetHandler resetHandler = new ResetHandler();
-	//	resetHandler.
 
 /**
 			Vehicle found = vehicleDAO.getByVIN("111-222-3333");
@@ -148,6 +190,7 @@ public class AddVehicle extends CITFragment {
 **/
 		}
 
+	// create clearFields method here and then call it from ResetHandler and AddHandler
 
 
 	public final class ResetHandler implements ClickListener {
