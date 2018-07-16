cec193749a1429dac777db455a651742c8f8ba8a
kameron_snell <sne11004@byui.edu>
2018-07-16 10:39:54 -0600

    debug database

--- Record/app/src/main/java/edu/byui/cit/record/home_lander.java
+++ Record/app/src/main/java/edu/byui/cit/record/home_lander.java
@@ -7,11 +7,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.ListView;
+import android.view.Menu;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.byui.cit.model.AppDatabase;
@@ -25,6 +29,11 @@ import edu.byui.cit.widget.WidgetWrapper;
 
 
 public class home_lander extends CITFragment {
+
+	ArrayList<String> listItems= new ArrayList<String>();
+	ArrayAdapter<String> adapter;
+	int clickCounter = 0;
+
 	@Override
 	protected View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstState){
 		View view = inflater.inflate(R.layout.home_lander, container, false);
@@ -55,10 +64,13 @@ public class home_lander extends CITFragment {
 		return view;
 	}
 
+
+
 	@Override
 	protected String getTitle() {
 		return "Record Home";
 	}
+
 
 	private final class newGoalClickHandler implements ClickListener{
 		@Override
@@ -66,5 +78,20 @@ public class home_lander extends CITFragment {
 			//calling parent method switchFragment
 			((MainActivity) getActivity()).switchFragment(new add_newGoal());
 		}
+	}
+
+	String[] mobileArray = {"goal1","goal2","goal3"};
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		getActivity().setContentView(R.layout.home_lander);
+		adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),android.R.layout.simple_list_item_1,mobileArray);
+		ListView listView = (ListView) getActivity().findViewById(R.id.toDoList);
+		listView.setAdapter(adapter);
+	}
+
+	public void addItems(View v){
+		listItems.add("Clicked : " +clickCounter++);
+		adapter.notifyDataSetChanged();
 	}
 }
