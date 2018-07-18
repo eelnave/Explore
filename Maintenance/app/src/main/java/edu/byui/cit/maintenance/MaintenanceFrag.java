602eb0fb95a5726d79d24a9b2d36a1caa5914243
benjamin_poore <924Cosmo17!>
2018-07-18 11:01:42 -0600

    Week 13
    1. Changed requestFocus for addVehicle frag
    2. Edited ChooseVehicle frag class so after clicking delete all the frag refreshes to reflect the removed vehicles
    3.1 Streamlined addVehicle by creating a callable method for clearing all fields.
    3.2 Called the clearFields(); method from addHandler and ResetHandler to clear all fields
    4. Added "other_frag" layout for additional maintenance
    5. Added "OtherFrag" class

--- Maintenance/app/src/main/java/edu/byui/cit/maintenance/MaintenanceFrag.java
+++ Maintenance/app/src/main/java/edu/byui/cit/maintenance/MaintenanceFrag.java
@@ -9,126 +9,152 @@ import android.widget.Button;
 
 import edu.byui.cit.widget.CITFragment;
 
-public class MaintenanceFrag extends CITFragment{
 
-    private OilFrag oilFrag;
-    private Button oil;
-    private BrakesFrag brakesFrag;
-    private Button Brakes;
-    private TiresFrag tiresFrag;
-    private Button Tires;
-    private BatteryFrag batteryFrag;
-    private Button Battery;
-    private AirFilterFrag airFilterFrag;
-    private Button airFilter;
+public class MaintenanceFrag extends CITFragment {
 
-
-    @Override
-    protected View createView(LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstState) {
-
-        View view = inflater.inflate(R.layout.maintenance_frag, container, false);
-
-        // create oil onClickListener
-        // prepend "view" to view.findViewById(R.id.oil); because you are outside of MainActivity
-        oil = view.findViewById(R.id.b_oil);
-        oil.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (oilFrag == null || oilFrag.isDetached()) {
-                    oilFrag = new OilFrag();
-                }
-                // hide FAB on fragment fragAct
-                //fab.hide();
-                //switch to fragment fragAct (for viewing vehicle details)
-                switchFragment(oilFrag);
-            }
-        });
+	private OilFrag oilFrag;
+	private Button oil;
+	private BrakesFrag brakesFrag;
+	private Button Brakes;
+	private TiresFrag tiresFrag;
+	private Button Tires;
+	private BatteryFrag batteryFrag;
+	private Button Battery;
+	private AirFilterFrag airFilterFrag;
+	private Button airFilter;
+	private OtherFrag otherFrag;
+	private Button other;
 
 
-        // create brakes onClickListener
-        // prepend "view" to view.findViewById(R.id.oil); because you are outside of MainActivity
-        Brakes = view.findViewById(R.id.b_brakes);
-        Brakes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (brakesFrag == null || brakesFrag.isDetached()) {
-                    brakesFrag = new BrakesFrag();
-                }
-                // hide FAB on fragment fragAct
-                //fab.hide();
-                //switch to fragment fragAct (for viewing vehicle details)
-                switchFragment(brakesFrag);
-            }
-        });
+	@Override
+	protected View createView(LayoutInflater inflater,
+			ViewGroup container, Bundle savedInstState) {
 
-        // create tires onClickListener
-        // prepend "view" to view.findViewById(R.id.oil); because you are outside of MainActivity
-        Tires = view.findViewById(R.id.b_tires);
-        Tires.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (tiresFrag == null || tiresFrag.isDetached()) {
-                    tiresFrag = new TiresFrag();
-                }
-                // hide FAB on fragment fragAct
-                //fab.hide();
-                //switch to fragment fragAct (for viewing vehicle details)
-                switchFragment(tiresFrag);
-            }
-        });
+		View view = inflater.inflate(R.layout.maintenance_frag, container,
+				false);
 
-        // create battery onClickListener
-        // prepend "view" to view.findViewById(R.id.oil); because you are outside of MainActivity
-        Battery = view.findViewById(R.id.b_battery);
-        Battery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (batteryFrag == null || batteryFrag.isDetached()) {
-                    batteryFrag = new BatteryFrag();
-                }
-                // hide FAB on fragment fragAct
-                //fab.hide();
-                //switch to fragment fragAct (for viewing vehicle details)
-                switchFragment(batteryFrag);
-            }
-        });
-
-        // create air filter onClickListener
-        // prepend "view" to view.findViewById(R.id.oil); because you are outside of MainActivity
-        airFilter = view.findViewById(R.id.b_air_filter);
-        airFilter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (airFilterFrag == null || airFilterFrag.isDetached()) {
-                    airFilterFrag = new AirFilterFrag();
-                }
-                // hide FAB on fragment fragAct
-                //fab.hide();
-                //switch to fragment fragAct (for viewing vehicle details)
-                switchFragment(airFilterFrag);
-            }
-        });
-
-        return view;
-    }
-
-    //added switchFragment for oil onClickListener
-    public void switchFragment(CITFragment fragment) {
-        // Replace whatever is in the fragContainer view with
-        // fragment, and add the transaction to the back stack so
-        // that the user can navigate back.
-        FragmentTransaction trans = getFragmentManager().beginTransaction();
-        trans.replace(R.id.fragContainer, fragment, "thing");
-        trans.addToBackStack(null);
-        trans.commit();
-    }
+		// create oil onClickListener
+		// prepend "view" to view.findViewById(R.id.oil); because you are
+        // outside of MainActivity
+		oil = view.findViewById(R.id.b_oil);
+		oil.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (oilFrag == null || oilFrag.isDetached()) {
+					oilFrag = new OilFrag();
+				}
+				// hide FAB on fragment fragAct
+				//fab.hide();
+				//switch to fragment fragAct (for viewing vehicle details)
+				switchFragment(oilFrag);
+			}
+		});
 
 
-    @Override
-    protected String getTitle() {
-        return "Maintenance";
-    }
+		// create brakes onClickListener
+		// prepend "view" to view.findViewById(R.id.oil); because you are
+        // outside of MainActivity
+		Brakes = view.findViewById(R.id.b_brakes);
+		Brakes.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (brakesFrag == null || brakesFrag.isDetached()) {
+					brakesFrag = new BrakesFrag();
+				}
+				// hide FAB on fragment fragAct
+				//fab.hide();
+				//switch to fragment fragAct (for viewing vehicle details)
+				switchFragment(brakesFrag);
+			}
+		});
+
+		// create tires onClickListener
+		// prepend "view" to view.findViewById(R.id.oil); because you are
+        // outside of MainActivity
+		Tires = view.findViewById(R.id.b_tires);
+		Tires.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (tiresFrag == null || tiresFrag.isDetached()) {
+					tiresFrag = new TiresFrag();
+				}
+				// hide FAB on fragment fragAct
+				//fab.hide();
+				//switch to fragment fragAct (for viewing vehicle details)
+				switchFragment(tiresFrag);
+			}
+		});
+
+		// create battery onClickListener
+		// prepend "view" to view.findViewById(R.id.oil); because you are
+        // outside of MainActivity
+		Battery = view.findViewById(R.id.b_battery);
+		Battery.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (batteryFrag == null || batteryFrag.isDetached()) {
+					batteryFrag = new BatteryFrag();
+				}
+				// hide FAB on fragment fragAct
+				//fab.hide();
+				//switch to fragment fragAct (for viewing vehicle details)
+				switchFragment(batteryFrag);
+			}
+		});
+
+		// create air filter onClickListener
+		// prepend "view" to view.findViewById(R.id.oil); because you are
+        // outside of MainActivity
+		airFilter = view.findViewById(R.id.b_air_filter);
+		airFilter.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (airFilterFrag == null || airFilterFrag.isDetached()) {
+					airFilterFrag = new AirFilterFrag();
+				}
+				// hide FAB on fragment fragAct
+				//fab.hide();
+				//switch to fragment fragAct (for viewing vehicle details)
+				switchFragment(airFilterFrag);
+			}
+		});
+
+		// create other maintenance onClickListener
+		// prepend "view" to view.findViewById(R.id.oil); because you are
+        // outside of MainActivity
+		other = view.findViewById(R.id.b_other_maintenance);
+		other.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (otherFrag == null || otherFrag.isDetached()) {
+					otherFrag = new OtherFrag();
+				}
+				// hide FAB on fragment fragAct
+				//fab.hide();
+				//switch to fragment fragAct (for viewing vehicle details)
+				switchFragment(otherFrag);
+			}
+		});
+
+		return view;
+	}
+
+	//added switchFragment for oil onClickListener
+	public void switchFragment(CITFragment fragment) {
+		// Replace whatever is in the fragContainer view with
+		// fragment, and add the transaction to the back stack so
+		// that the user can navigate back.
+		FragmentTransaction trans = getFragmentManager().beginTransaction();
+		trans.replace(R.id.fragContainer, fragment, "thing");
+		trans.addToBackStack(null);
+		trans.commit();
+	}
+
+
+	@Override
+	protected String getTitle() {
+		return "Maintenance";
+	}
 
 
 }
