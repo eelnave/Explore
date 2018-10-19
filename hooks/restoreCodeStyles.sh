37b8328cbb05c130e245d509a4023250521c1d9d
BarzeeRex <barzeer@byui.edu>
2018-10-19 20:48:04 +0100

    Rename icon_bodyOfWater to icon_bodyofwater because Android Studio does not allow upper case letters in resource file names.

--- hooks/restoreCodeStyles.sh
+++ hooks/restoreCodeStyles.sh
@@ -4,7 +4,7 @@ PROJECTS="Calc360 DateApp Explore Fishing kindnessClient Maintenance Record"
 # PROJECTS="BasicJava Classes Collections Console Exceptions FileIO Simple"
 
 for proj in ${PROJECTS} ; do
-	cp ../apps/hooks/codeStyles/* ${proj}/.idea/codeStyles
+	cp  hooks/codeStyles/* ${proj}/.idea/codeStyles
 	git add -f ${proj}/.idea/codeStyles/*
 done
 
