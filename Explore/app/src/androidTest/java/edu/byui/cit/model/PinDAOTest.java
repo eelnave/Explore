64275f6134490025be826caaaad82145f0064dc5
JadeJensen <jen15010@byui.edu>
2018-10-26 16:21:58 -0600

    Created additional tests to verify results.

--- Explore/app/src/androidTest/java/edu/byui/cit/model/PinDAOTest.java
+++ Explore/app/src/androidTest/java/edu/byui/cit/model/PinDAOTest.java
@@ -28,13 +28,23 @@ public class PinDAOTest{
 	}
 
 	Date date = new Date();
-	Pin pin0 = new Pin(1,"pin",10.0,10.0,date,"notes");
+	Pin pin0 = new Pin(1,"pin",10.0,15.0,date,"notes");
+	Double delta = 0.00001;
 	@Test
 	public void getAll() {
 		pinDAO.insert(pin0);
-		long id = pin0.getPinId();
-		assertTrue(id > 0);
+		// check pin0 assertions
+		assertTrue(pin0.getPinId() > 0);
+		assertEquals(1,pin0.getPinId());
+		assertTrue(pin0.getIconName() == "pin");
+		assertEquals("pin", pin0.getIconName());
 		assertTrue(pin0.getLatitude() == 10.0);
+		assertEquals(10.0, (double)pin0.getLatitude(), delta);
+		assertTrue(pin0.getLongitude() == 15.0);
+		assertEquals(15.0, (double)pin0.getLongitude(), delta);
+		assertEquals(date,pin0.getDate());
+		assertTrue(pin0.getNotes() == "notes");
+		// test database assertions
 
 		List<Pin> all = pinDAO.getAll();
 		assertEquals(1, all.size());
