dc67121ec7c76b24f94b4968f901ad17cb1d1eb3
Zac Bell <bel05023@byui.edu>
2018-11-12 11:00:57 -0700

    add DAO rough draft and fly data class

---
+++ Fishing/app/src/main/java/edu/byui/cit/fishing/model/FlyData.java
@@ -0,0 +1,4 @@
+package edu.byui.cit.fishing.model;
+
+public class FlyData {
+}
---
+++ Fishing/app/src/main/java/edu/byui/cit/fishing/model/FlyDataDAO.java
@@ -0,0 +1,22 @@
+package edu.byui.cit.fishing.model;
+
+import java.util.List;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+@Dao
+public interface FlyDataDAO {
+	@Query("SELECT * FROM FlyData")
+	List<Fly> getAll();
+
+	@Insert
+	void insert(FlyData fly);
+
+	@Delete
+	void delete(FlyData fly);
+
+	@Query("DELETE FROM FlyData")
+	void clearTable();
+}
