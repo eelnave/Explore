78cf134028ab17cd3ae6b0748a205da0ffb9702d
janaefairbourn <36008469+janaefairbourn@users.noreply.github.com>
2018-12-15 15:10:41 -0700

    Created a splash screen

---
+++ Explore/app/src/main/java/edu/byui/cit/explore/Splashscreen.java
@@ -0,0 +1,81 @@
+package edu.byui.cit.explore;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/*
+Brings up the wolf explore image when you are first loading the app.
+Files included in this code are: res/anim/alpha.xml, res/anim/translate.xml,
+res/layout/activity_splashscreen.xml, and res/drawable/splash_img.png
+If you wish to change the image, go to the drawable folder and replace
+splash_img.png You will need to name your new picture the same, or, go to
+activity_splashscreen.xml and change the last line of code
+(android:background="@drawable/splash_img" />) so that the name of you picture
+replaces spash_img.  You'll also find you can change the background color
+and positioning on activity_splashscreen.xml.
+To change how long the image stays on the screen you can change the settings
+in alpha.xml and translate.xml.
+ */
+
+public class Splashscreen extends Activity {
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+    /** Starts when the app is opened */
+    Thread splashTread;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        StartAnimations();
+    }
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l=(LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.splash);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(Splashscreen.this,
+                            MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    Splashscreen.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    Splashscreen.this.finish();
+                }
+
+            }
+        };
+        splashTread.start();
+
+    }
+
+}
